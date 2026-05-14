014a5f9ab5a6a3d45f4a0dd0355c0da394ed5fcb
Antholl73 <anthonyavilyo@gmail.com>
2026-05-14 11:25:19 -0500

    creada tabla usuario

--- Tabla_entidades.sql
+++ Tabla_entidades.sql
@@ -2,3 +2,9 @@ CREATE TABLE discografia (
 id_discografia SERIAL PRIMARY KEY,
 nombre VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE usuario(
+id_usuario SERIAL PRIMARY KEY,
+correo VARCHAR(50) UNIQUE KEY,
+nombre VARCHAR(50) NOT NULL
+);
