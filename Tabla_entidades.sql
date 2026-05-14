84f04d108565cd01facef4c180ee42fd39e2d496
Antholl73 <anthonyavilyo@gmail.com>
2026-05-14 11:16:31 -0500

    creada la tabla discografía

--- Tabla_entidades.sql
+++ Tabla_entidades.sql
@@ -1,6 +1,4 @@
-CREATE TABLE empleados (
-id SERIAL PRIMARY KEY,
-nombre VARCHAR(50) NOT NULL,
-apellido VARCHAR(50) NOT NULL,
-correo VARCHAR(100) UNIQUE,
-fecha_nmiento DATE)
+CREATE TABLE discografia (
+id_discografia SERIAL PRIMARY KEY,
+nombre VARCHAR(50) NOT NULL;
+)
