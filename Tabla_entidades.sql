555b330be6b4f77af0a4806338353689f9d7ada3
fabian0rtiz <fabianortiz0305@gmail.com>
2026-05-14 17:14:37 -0500

    agregue el genero y corregi usuario en correo , unique key -> unique

--- Tabla_entidades.sql
+++ Tabla_entidades.sql
@@ -1,10 +1,14 @@
-CREATE TABLE discografia (
 id_discografia SERIAL PRIMARY KEY,
 nombre VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE usuario(
 id_usuario SERIAL PRIMARY KEY,
-correo VARCHAR(50) UNIQUE KEY,
+correo VARCHAR(50) UNIQUE ,
+nombre VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE genero(
+id_genero serial PRIMARY KEY,
 nombre VARCHAR(50) NOT NULL
 );
