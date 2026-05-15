f22069fecd8eaeb217ed4bed4e0b8246a2481d85
fabian0rtiz <fabianortiz0305@gmail.com>
2026-05-14 21:36:28 -0500

    agregue las canciones

--- Tabla_entidades.sql
+++ Tabla_entidades.sql
@@ -20,23 +20,37 @@ nombre VARCHAR(50) NOT NULL
 );
 
 
+
 CREATE TABLE artistas(
 id_artista VARCHAR(50) PRIMARY KEY,
 nombre VARCHAR(50) Not NULL
 );
 
 CREATE TABLE albumes(
-    id_album VARCHAR(50) PRIMARY KEY,
-    
-    id_artista VARCHAR(50),
-    id_discografia INT,
+  id_album VARCHAR(50) PRIMARY KEY,
+  id_artista VARCHAR(50),
+  id_discografia INT,
+  nombre_album VARCHAR(50),
+  fecha_lanzamiento DATE,
+  
+  FOREIGN KEY (id_artista)
+  REFERENCES artistas(id_artista),
 
-    nombre_album VARCHAR(50),
-    fecha_lanzamiento DATE,
+  FOREIGN KEY (id_discografia)
+  REFERENCES discografias(id_discografia)
+);
 
-    FOREIGN KEY (id_artista)
-        REFERENCES artistas(id_artista),
 
-    FOREIGN KEY (id_discografia)
-        REFERENCES discografias(id_discografia)
+CREATE TABLE canciones(
+id_cancion SERIAL PRIMARY KEY,
+nombre VARCHAR(50) NOT NULL,
+duracion float,
+id_album VARCHAR(50),
+id_genero INT,
+
+FOREIGN KEY (id_album)
+REFERENCES albumes(id_album),
+FOREIGN KEY (id_genero)
+REFERENCES generos(id_genero)
+
 );
