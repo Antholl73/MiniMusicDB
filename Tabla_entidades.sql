ba46d551b5dd1fd7d68ea7a41398be5492282bfc
Antholl73 <anthonyavilyo@gmail.com>
2026-05-14 19:46:02 -0500

    añadida la entidad "idiomas", fix: puse en plural las entidades

--- Tabla_entidades.sql
+++ Tabla_entidades.sql
@@ -1,17 +1,22 @@
-CREATE TABLE discografia (
+CREATE TABLE discografias (
 id_discografia SERIAL PRIMARY KEY,
 nombre VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE usuario(
+CREATE TABLE usuarios(
 id_usuario SERIAL PRIMARY KEY,
 correo VARCHAR(50) UNIQUE ,
 nombre VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE genero(
+CREATE TABLE generos(
 id_genero serial PRIMARY KEY,
 nombre VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE idiomas(
+id_idioma VARCHAR(50) PRIMARY KEY,
+nombre VARCHAR(50) NOT NULL
+)
 
 select * from genero;
