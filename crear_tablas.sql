aeb6a8114786a71af1ffc8f4aebe512a5e8010a3
fabian0rtiz <fabianortiz0305@gmail.com>
2026-05-18 16:28:27 -0500

    correcion de ','

--- crear_tablas.sql
+++ crear_tablas.sql
@@ -103,7 +103,7 @@ id SERIAL, --FIX: PRIMARY KEY va abajo
 id_usuario INTEGER,
 id_cancion INTEGER,
 texto TEXT NOT NULL,
-fecha_comentario TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+fecha_comentario TIMESTAMP DEFAULT CURRENT_TIMESTAMP,--fix::faltaba una coma
 
 PRIMARY KEY(id_usuario, id_cancion, id), --FIX:: la PRIMARY KEY debería ser una combinación de id_usuario, id_canción, id_comentario
 
