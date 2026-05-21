daf7f9eedbb8b93db7687faeac53dde199e85681
Antholl73 <anthonyavilyo@gmail.com>
2026-05-21 10:00:46 -0500

    agregando unos valores a la tabla generos

--- insersiones.sql
+++ insersiones.sql
@@ -2,3 +2,12 @@
 --Mínimo 5 registros por tabla para cumplir la rúbrica.
 --¡Cuidado con el orden! No pueden poblar una tabla hija (ej: Pedidos) si antes no han poblado la tabla
 --padre (Clientes), debido a las Llaves Foráneas.
+
+INSERT INTO generos(nombre) VALUES('salsa');
+INSERT INTO generos(nombre) VALUES('reggueaton');
+INSERT INTO generos(nombre) VALUES('pop');
+INSERT INTO generos(nombre) VALUES('punkmetal');
+INSERT INTO generos(nombre) VALUES('metal');
+INSERT INTO generos(nombre) VALUES('heavy metal');
+INSERT INTO generos(nombre) VALUES('rock');
+
