5db9c0b9196a710f87a36c28ba4651a4e30765b7
Rodrigo Renato Gamero Bocangel <rrgbnormal@gmail.com>
2026-05-22 23:39:44 -0500

    fix: se corrigio error de logica que cambiaria todas las canciones en ves de la unica asociada a la playlist

--- inserciones.sql
+++ inserciones.sql
@@ -112,11 +112,11 @@ WHERE id = 1 ;
 BEGIN;
 UPDATE detalles_playlist 
 SET posicion = 2
-WHERE id_cancion = 1;
+WHERE id_playlist = 1 AND id_cancion = 1;
 
 UPDATE detalles_playlist 
 SET posicion = 1
-WHERE id_cancion = 2;
+WHERE id_playlist = 1 AND id_cancion = 2;
 COMMIT;
 
 --modificacion discograficas
