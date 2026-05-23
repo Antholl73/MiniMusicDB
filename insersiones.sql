04c101da214554a11da0666090cdf427ed07eb00
fabian0rtiz <fabianortiz0305@gmail.com>
2026-05-22 20:54:12 -0500

    modificacion de artistas

--- insersiones.sql
+++ insersiones.sql
@@ -84,3 +84,9 @@ SET correo = 'eljuan_alcachofa@gmail.com'
 WHERE nombre = 'juan';
 
 
+--modificacion a artistas
+UPDATE artistas
+SET nombre='El Rey del Pop'  
+WHERE nombre='Michael Jackson';
+
+
