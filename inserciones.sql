136d60abb48766e3acc95585c018b306e85900ed
fabian0rtiz <fabianortiz0305@gmail.com>
2026-05-29 19:55:36 -0500

    agregando algunos fragmentos de letras de canciones

--- inserciones.sql
+++ inserciones.sql
@@ -138,13 +138,18 @@ INSERT INTO albumes(id, id_artista, id_discografica, nombre_album, fecha_lanzami
 (6,6,6,'Hybrid Theory','2000-10-24'),
 (7,7,7,'Parachutes','2000-07-10'),
 (8,8,8,'21','2011-01-24');
---agregando mas canciones
+
 INSERT INTO canciones (id_album,id_artista,id_genero,nombre,duracion) VALUES
 (5,5,5,'Sonne',4.32),
 (6,6,8,'In the End',3.36),
 (7,7,7,'Yellow',4.28),
 (8,8,3,'Rolling in the Deep',3.48);
-
+--agregando algunas letras_cancion( solo son unas partes)
+INSERT INTO letras_cancion(id_cancion,id_idioma,contenido) VALUES
+(5,7,'Hier kommt die Sonne...'),
+(6,2,'It starts with one thing...'),
+(7,2,'Look at the stars...'),
+(8,2,'There is a fire starting in my heart...');
 --Modificacion
 
 --modificacion playlist
