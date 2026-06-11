35137ca26367a579230eadac3bd740d12ddd4edd
rodrigo <rrgbnormal@gmail.com>
2026-06-11 09:18:39 -0500

    feat: añadidos more deletes a canciones

--- deletes/delete_cancion.sql
+++ deletes/delete_cancion.sql
@@ -1,2 +1,11 @@
 DELETE FROM canciones
-WHERE nombre = "Homura"
+WHERE nombre = "Homura";
+
+DELETE FROM canciones
+WHERE nombre = "Sonne";
+
+DELETE FROM canciones
+WHERE nombre = "Bad Guy";
+
+DELETE FROM canciones
+WHERE nombre = "Get Lucky"
