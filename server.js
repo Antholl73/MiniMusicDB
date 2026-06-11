f17317551166080ecb5cadc21e8bde9027ddaf15
fabian0rtiz <fabianortiz0305@gmail.com>
2026-06-11 06:57:18 -0500

    agregando 2 reportes

--- server.js
+++ server.js
@@ -290,6 +290,36 @@ app.get('/api/reportes/total-canciones-por-genero/csv', async (req, res) => {
   }
 });
 
+//canctidad de canciones por album
+app.get('/api/reportes/total-canciones-por-album/csv', async (req, res) => {
+  try {
+    const result = await pool.query(`
+      SELECT id_album, COUNT(*) AS total_canciones
+      FROM canciones
+      GROUP BY id_album
+    `);
+
+    let csv = 'id_album,total_canciones\n';
+
+    result.rows.forEach(row => {
+      csv += `${row.id_album},${row.total_canciones}\n`;
+    });
+
+    res.setHeader('Content-Type', 'text/csv');
+    res.setHeader(
+      'Content-Disposition',
+      'attachment; filename="canciones_por_album.csv"'
+    );
+
+    res.send(csv);
+
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      error: error.message
+    });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
