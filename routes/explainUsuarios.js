11e5347c8ac3acbade8aebe8b89358f3b0f4cc88
Rodrigo Renato Gamero Bocangel <rrgbnormal@gmail.com>
2026-06-18 04:45:31 -0500

    fix: explain reducido para mostrar solo lo importante (index o seq)

--- routes/explainUsuarios.js
+++ routes/explainUsuarios.js
@@ -7,13 +7,13 @@ router.get('/api/explainUsuarios/:email', async (req, res) => {
     const { email } = req.params;
 
     const result = await pool.query(`
-      EXPLAIN (ANALYZE, BUFFERS, FORMAT JSON)
+      EXPLAIN (ANALYZE, BUFFERS)
       SELECT *
       FROM usuarios
       WHERE correo = $1
     `, [email]);
 
-    res.json(result.rows[0]);
+    res.json(result.rows);
 
   } catch (error) {
     console.error(error);
