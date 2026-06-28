4041e7bea6797eff02d5214f82a0845edeb7eb4d
Antholl73 <anthonyavilyo@gmail.com>
2026-06-28 18:46:53 -0500

    modularizando cancionesPopularesGenero

--- server.js
+++ server.js
@@ -32,44 +32,6 @@ app.get('/api/db-test', async (req, res) => {
   }
 });
 
-// Canciones del genero 3 con calificacion promedio >= 3
-app.get('/api/canciones/genero/:id_genero/bien-calificadas/csv', async (req, res) => {
-  const { id_genero } = req.params;
-
-  try {
-    const result = await pool.query(`
-      SELECT
-        canciones.nombre AS nombre_cancion,
-        AVG(calificaciones.valoracion) AS calificacion_promedio
-      FROM canciones
-      LEFT JOIN calificaciones ON canciones.id = calificaciones.id_cancion
-      WHERE canciones.id_genero = $1
-      GROUP BY canciones.nombre, canciones.id
-      HAVING AVG(calificaciones.valoracion) >= 3
-    `, [id_genero]);
-
-    // construir CSV
-    let csv = 'nombre_cancion,calificacion_promedio\n';
-
-    result.rows.forEach(row => {
-      csv += `${row.nombre_cancion},${row.calificacion_promedio}\n`;
-    });
-
-    //headers para descarga
-    res.header('Content-Type', 'text/csv');
-    res.attachment(`canciones_genero_${id_genero}.csv`);
-
-    res.send(csv);
-
-  } catch (error) {
-    res.status(500).json({
-      ok: false,
-      error: error.message,
-      codigo: error.code
-    });
-  }
-});
-
 
 const rutaTablas = require('./routes/tablas');
 app.use('/api/total-tablas', rutaTablas);
@@ -92,6 +54,7 @@ app.use(require('./routes/cancionesporGenero'));
 app.use(require('./routes/totalUsuarios'));
 app.use(require('./routes/aniadircancionAlbum'));
 app.use(require('./routes/generosReproducciones'));
+app.use(require('./routes/cancionesPopularesGenero'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
