8b926a2653d127d9ff688dc47f8e32b5c0d5ac9d
Antholl73 <anthonyavilyo@gmail.com>
2026-06-28 18:16:45 -0500

    modularizando crearPlaylist endpoint

--- server.js
+++ server.js
@@ -288,54 +288,6 @@ app.get('/api/reportes/total-canciones-por-genero/csv', async (req, res) => {
 
 
 
-app.post('/api/playlists/crear', async (req, res) => {
-  const { nombre, canciones } = req.body;
-
-  const client = await pool.connect();
-
-  try {
-    await client.query('BEGIN');
-
-    // Crear playlist
-    const playlist = await client.query(`
-      INSERT INTO playlists (tipo, nombre)
-      VALUES ('personalizada', $1)
-      RETURNING id
-    `, [nombre]);
-
-    const id_playlist = playlist.rows[0].id;
-
-    // Agregar canciones
-    for (let i = 0; i < canciones.length; i++) {
-      await client.query(`
-        INSERT INTO detalles_playlist
-        (id_playlist, id_cancion, posicion)
-        VALUES ($1, $2, $3)
-      `, [id_playlist, canciones[i], i + 1]);
-    }
-
-    await client.query('COMMIT');
-
-    res.json({
-      ok: true,
-      mensaje: 'Playlist creada correctamente'
-    });
-
-  } catch (error) {
-    await client.query('ROLLBACK');
-
-    res.status(500).json({
-      ok: false,
-      mensaje: 'No se pudo crear la playlist',
-      error: error.message
-    });
-
-  } finally {
-    client.release();
-  }
-});
-
-
 const rutaTablas = require('./routes/tablas');
 app.use('/api/total-tablas', rutaTablas);
 
@@ -351,7 +303,9 @@ app.use(require('./routes/crearIndiceHistoriales'));
 app.use(require('./routes/borrarIndiceHistoriales'));
 
 app.use(require('./routes/registrarCancion'));
-app.use(require('./routes/cancionesporAlbum'))
+app.use(require('./routes/cancionesporAlbum'));
+app.use(require('./routes/crearPlaylist'));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
